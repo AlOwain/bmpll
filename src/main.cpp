03f2a1c59b70a33e36a2967e7a1d8513f52dbb79
AlOwain <abdulaziz@alowain.dev>
2023-12-27 00:22:04 +0300

    Removed unnecessary prints

--- src/main.cpp
+++ src/main.cpp
@@ -5,18 +5,7 @@ int main(int argc, char** argv)
 {
     rule_setter y("input/example.rules");
 
-    bmp x("input/example-simple.bmp");
+    bmp x("input/example.bmp");
 
-    for (int i = 0; i < x.get_height(); i++)
-    {
-        for (int j = 0; j < x.get_width(); j++)
-        {
-            std::cout << "("
-                    << (int) x.get_pixel(i, j).r << " "
-                    << (int) x.get_pixel(i, j).g << " "
-                    << (int) x.get_pixel(i, j).b << ") ";
-        }
-        std::cout << "\n";
-    }
     return 0;
 }
